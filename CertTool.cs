7d9bc62e2567dd599a0ab05fa5d99df06f4b9030
Zalmat <zalmat@yandex.ru>
2021-01-05 20:36:02 +0500

    Скрыты лишние зависимости.

--- CertTool.cs
+++ CertTool.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
+//using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
+//using System.Text;
 using System.Text.RegularExpressions;
 
 namespace TaskExcecutorProcess
